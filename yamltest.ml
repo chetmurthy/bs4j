8cff3a393e545ea256c6d86a490c983c5fbaacb3
Chet Murthy <chetsky@gmail.com>
2021-03-14 14:16:44 -0700

    adding examples

--- yamltest.ml
+++ yamltest.ml
@@ -2,6 +2,8 @@ open OUnit2
 open OUnitTest
 open Yaml
 
+let warning s = Fmt.(pf stderr "%s\n%!" s)
+
 type value =
   [ `A of value list
   | `Bool of bool
@@ -81,6 +83,66 @@ national:
   name: Sammy Sosa
   hr:   63
   avg:  0.288|})
+      )
+  ; "2.5" >:: (fun ctxt ->
+        assert_equal ~printer
+          (Ok(`A (
+               [`A ([`String ("name"); `String ("hr"); `String ("avg")]);
+                `A ([`String ("Mark McGwire"); `Float (65.); `Float (0.278)]);
+                `A ([`String ("Sammy Sosa"); `Float (63.); `Float (0.288)])]
+             )))
+        (of_string {|- [name        , hr, avg  ]
+- [Mark McGwire, 65, 0.278]
+- [Sammy Sosa  , 63, 0.288]
+
+|})
+      )
+  ; "2.6" >:: (fun ctxt ->
+        assert_equal ~printer
+          (Ok(`O (
+               [("Mark McGwire", `O ([("hr", `Float (65.)); ("avg", `Float (0.278))]));
+                ("Sammy Sosa", `O ([("hr", `Float (63.)); ("avg", `Float (0.288))]))]
+             )))
+        (of_string {|Mark McGwire: {hr: 65, avg: 0.278}
+Sammy Sosa: {
+    hr: 63,
+    avg: 0.288
+  }|})
+      )
+  ; "2.7" >:: (fun ctxt ->
+      warning "example 2.7 has multiple docs: this isn't implemented right" ;
+        assert_equal ~printer
+          (Ok(`A (
+               [`String ("Mark McGwire"); `String ("Sammy Sosa"); `String ("Ken Griffey")]
+             )))
+        (of_string {|# Ranking of 1998 home runs
+---
+- Mark McGwire
+- Sammy Sosa
+- Ken Griffey
+
+# Team ranking
+---
+- Chicago Cubs
+- St Louis Cardinals|})
+      )
+  ; "2.8" >:: (fun ctxt ->
+      warning "example 2.8 has multiple docs: this isn't implemented right" ;
+        assert_equal ~printer
+          (Ok(`O (
+               [("time", `String ("20:03:20")); ("player", `String ("Sammy Sosa"));
+                ("action", `String ("strike (miss)"))]
+             )))
+        (of_string {|---
+time: 20:03:20
+player: Sammy Sosa
+action: strike (miss)
+...
+---
+time: 20:03:47
+player: Sammy Sosa
+action: grand slam
+...|})
       )
 
   ]
