845f2f8f4073bb34c05f54141a4e9d35a38ce8d6
Chet Murthy <chetsky@gmail.com>
2021-03-14 13:59:03 -0700

    adding examples

--- yamltest.ml
+++ yamltest.ml
@@ -36,6 +36,52 @@ let examples = "examples" >::: [
 avg: 0.278 # Batting average
 rbi: 147   # Runs Batted In|})
       )
+  ; "2.3" >:: (fun ctxt ->
+        assert_equal ~printer
+          (Ok(`O (
+               [("american",
+                 `A (
+                   [`String ("Boston Red Sox"); `String ("Detroit Tigers");
+                    `String ("New York Yankees")]
+                 ));
+                ("national",
+                 `A (
+                   [`String ("New York Mets"); `String ("Chicago Cubs");
+                    `String ("Atlanta Braves")]
+                 ))
+               ]
+             )))
+        (of_string {|american:
+  - Boston Red Sox
+  - Detroit Tigers
+  - New York Yankees
+national:
+  - New York Mets
+  - Chicago Cubs
+  - Atlanta Braves|})
+      )
+  ; "2.4" >:: (fun ctxt ->
+        assert_equal ~printer
+          (Ok(`A (
+               [`O (
+                   [("name", `String ("Mark McGwire")); ("hr", `Float (65.));
+                    ("avg", `Float (0.278))]
+                 );
+                `O (
+                  [("name", `String ("Sammy Sosa")); ("hr", `Float (63.));
+                   ("avg", `Float (0.288))]
+                )
+               ]
+             )))
+        (of_string {|-
+  name: Mark McGwire
+  hr:   65
+  avg:  0.278
+-
+  name: Sammy Sosa
+  hr:   63
+  avg:  0.288|})
+      )
 
   ]
 
